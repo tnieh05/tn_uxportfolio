afa17fb60b142ff9d4b41466c75635be19c7f66a
Tiffany <tnieh@tradecrafted.com>
2017-11-01 15:42:58 -0700

    portfolio redesign

--- js/parallax.js
+++ js/parallax.js
@@ -7,11 +7,16 @@ function parallax() {
     $(".parallax-bg").css("background-position", (wScroll*0.25)+"px")
 }*/
 
-
+jQuery(document).ready(function($) {
+    
 $(document).on('click', 'a[href*="#"]', function(event){
     event.preventDefault();
 
     $('html, body').animate({
         scrollTop: $( $.attr(this, 'href') ).offset().top
     }, 600);
+});
+
+
+    
 });
